f7375c89d45a7a879fab186b8609a0bf10f63489
sirjordantw <jordantw128@gmail.com>
2026-04-16 11:22:00 -0400

    .

--- proj3/src/main.cc
+++ proj3/src/main.cc
@@ -1,8 +1,19 @@
 // Copyright 2026 Jordan Weinstein
-#include "lib/include/mmap.h"
+// Libraries
+#include <stdio.h>
+#include <string.h>
+
+// Headers
+#include "proj3/lib/include/mmap.h"
 
 int main(int argc, char* argv[]) {
-    
+    if (strcmp(argv[1], "create") == 0) {
+        
+    } else if (strcmp(argv[1], "insert") == 0) {
+
+    } else if (strcmp(argv[1], "append") == 0) {
+
+    }
 
     return 0;
 }
